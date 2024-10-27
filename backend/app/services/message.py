ed684c86f657e9a10168d7306fa6dfa8ac7c952b
jinyang628 <chenjinyang4192@gmail.com>
2024-10-27 11:48:49 -0700

    Update

--- backend/app/services/message.py
+++ backend/app/services/message.py
@@ -4,24 +4,21 @@ from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright
 
 from app.models.generate import GenerateResponse
-
+from app.models.message import Message
 
 class MessageService:
     async def generate(self, url: str) -> GenerateResponse:
-        html: str = await extract_html(url)
-        processed_content: str = process_html(html)
+        atree: dict = await extract_atree(url)
+        processed_content: str = process_atree(atree)
         with open("atree.html", "w") as f:
             f.write(processed_content)
         print(processed_content)
 
 
-def process_html(html: str) -> str:
-    soup = BeautifulSoup(html, "html.parser")
-    submit_box = soup.find("div", class_="submit-box")
-    return str(submit_box) if submit_box else ""
+def process_atree(atree: dict) -> list[Message]:
+    print(atree.get("children"))
 
-
-async def extract_html(url: str) -> str:
+async def extract_atree(url: str) -> dict:
     # We are using this chrome extension to extract the claude chat log
     # https://chromewebstore.google.com/detail/ai-archives-share-claude/jagobfpimhagccjbkchfdilhejgfggna?hl=en
     if not url.startswith("https://aiarchives.org/id/"):
@@ -32,6 +29,6 @@ async def extract_html(url: str) -> str:
         page = await browser.new_page()
         await page.goto(url)
         time.sleep(5)  # Wait for page ready is not reliable
-        html_content = await page.content()
+        atree = await page.accessibility.snapshot() or {}
         await browser.close()
-        return html_content
+        return atree
